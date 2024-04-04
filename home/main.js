3c37bea458e4459d14ddd7558d13b39f666e1d7b
nhatthanhdev <phamnhatthanh2212@gmail.com>
2024-04-05 02:45:35 +0700

    hiuhiu

--- home/main.js
+++ home/main.js
@@ -1,7 +1,11 @@
 var new_task = document.querySelector(".new-task");
 const inputs = document.querySelectorAll('input');
 const textArea = document.querySelector('textarea');
-const submit=document.getElementById('submit');
+const submit = document.getElementById('submit');
+const todo = document.getElementById('todo');
+const doing=document.getElementById('doing');
+const completed=document.getElementById('completed');
+const blocked=document.getElementById('blocked');
 new_task.addEventListener('click', () => {
     document.querySelector(".box").style.display = 'block';
     document.body.style.overflow = 'hidden';
@@ -14,36 +18,37 @@ document.querySelector(".box-add").addEventListener('click', () => {
 function closeBox() {
     document.querySelector(".box").style.display = 'none';
     document.body.style.overflow = 'auto';
-    inputs.forEach(input=>{
-        input.classList=[];
-        input.value='';
+    inputs.forEach(input => {
+        input.classList = [];
+        input.value = '';
     });
-    textArea.classList=[];
-    textArea.value='';
+    textArea.classList = [];
+    textArea.value = '';
 }
 inputs.forEach((input) => {
     input.addEventListener('blur', () => {
         checkInput(input);
     })
 })
-textArea.addEventListener('blur',()=>{
+textArea.addEventListener('blur', () => {
     checkInput(textArea)
 })
-submit.addEventListener('click',()=>{
-    if(checkSubmit()){
+submit.addEventListener('click', () => {
+    if (checkSubmit()) {
         addTodo();
+        refresh();
         closeBox();
     }
 })
-function checkSubmit(){
-    let inputValid=true;
-    inputs.forEach(input=>{
-        if(!checkInput(input)){
-            inputValid=false;
+function checkSubmit() {
+    let inputValid = true;
+    inputs.forEach(input => {
+        if (!checkInput(input)) {
+            inputValid = false;
         }
     })
-    let textAreaValid=checkInput(textArea);
-    return inputValid&&textAreaValid;
+    let textAreaValid = checkInput(textArea);
+    return inputValid && textAreaValid;
 }
 function checkInput(input) {
     const text = input.value;
@@ -58,68 +63,100 @@ function checkInput(input) {
     }
 }
 
-function getListTodo(){
-    if (localStorage.getItem("todo")) {
-        return JSON.parse(localStorage.getItem("todo"));
+function getListTasks() {
+    if (localStorage.getItem("tasks")) {
+        return JSON.parse(localStorage.getItem("tasks"));
     } else {
         return [];
     }
 }
-function saveTodo(todoList){
-    localStorage.setItem('todo',JSON.stringify(todoList))
+function saveTask(todoList) {
+    localStorage.setItem('tasks', JSON.stringify(todoList))
 }
-function getListDoing(){
-    if (localStorage.getItem("doing")) {
-        return JSON.parse(localStorage.getItem("doing"));
-    } else {
-        return [];
-    }
-}
-function saveDoing(todoList){
-    localStorage.setItem('doing',JSON.stringify(todoList))
-}
-function getListCompleted(){
-    if (localStorage.getItem("completed")) {
-        return JSON.parse(localStorage.getItem("completed"));
-    } else {
-        return [];
-    }
-}
-function saveCompleted(todoList){
-    localStorage.setItem('completed',JSON.stringify(todoList))
-}
-function getListBlocked(){
-    if (localStorage.getItem("blocked")) {
-        return JSON.parse(localStorage.getItem("blocked"));
-    } else {
-        return [];
-    }
-}
-function saveBlocked(todoList){
-    localStorage.setItem('blocked',JSON.stringify(todoList))
-}
+
 function formatDate(timestamp) {
     const date = new Date(timestamp);
-
-    const options = { 
+    const options = {
         year: 'numeric',
-        month: 'long', 
-        day: 'numeric' 
+        month: 'long',
+        day: 'numeric'
     };
     return date.toLocaleDateString('en-US', options);
 }
-function Task(category,title,content,datetime){
-    this.category=category;
-    this.title=title;
-    this.content=content;
-    this.datetime=formatDate(datetime);
+const statusTask={
+    Todo:"Todo",
+    Doing:"Doing",
+    Completed:"Completed",
+    Blocked:"Blocked"
+};
+function Task(idTask,category, title, content, datetime,taskStatus) {
+    this.idTask=idTask;
+    this.category = category;
+    this.title = title;
+    this.content = content;
+    this.datetime = formatDate(datetime);
+    this.taskStatus=taskStatus;
 }
-function addTodo(){
-    const category=document.getElementById('category').value;
-    const title=document.getElementById('title').value;
-    const content=document.getElementById('content').value;
-    const task= new Task(category,title,content,Date.now());
-    const todoList=getListTodo();
+function addTodo() {
+    const todoList = getListTasks();
+    const category = document.getElementById('category').value;
+    const title = document.getElementById('title').value;
+    const content = document.getElementById('content').value;
+    const task = new Task(todoList.length,category, title, content, Date.now(),statusTask.Todo);
     todoList.push(task);
-    saveTodo(todoList);
+    saveTask(todoList);
+}
+
+var itemTemplate = (data) => {
+    return `<div class="task-item">
+        <div class="task-title">
+        <div>
+            <span class="task-category">${data.category}</span>
+            <h3>${data.title}</h3>
+        </div>
+        <div class="task-action">
+            <img src="/asset/update.png" alt="update">
+            <img src="/asset//delete.png" alt="delete" id="delete" onclick=deltask(${data.idTask})>
+        </div>
+    </div>
+    <div class="task-des">
+        <p>${data.content}</p>
+        <span class="time"><img src="/asset/clock.png" alt="clock">${data.datetime}</span>
+    </div>
+</div>`
+}
+function refresh() {
+    var txt_todo="";
+    var txt_doing="";
+    var txt_completed="";
+    var txt_blocked="";
+    const task = getListTasks();
+    task.forEach((t) => {
+        switch (t.taskStatus){
+            case "Todo":
+                txt_todo+=itemTemplate(t);
+                break;
+            case "Doing":
+                txt_doing+=itemTemplate(t);
+                break;
+            case "Completed":
+                txt_completed+=itemTemplate(t);
+                break;
+            case "Blocked":
+                txt_blocked+=itemTemplate(t);
+                break;
+        }
+    })
+    todo.innerHTML=txt_todo;
+    doing.innerHTML=txt_doing;
+    completed.innerHTML=txt_completed;
+    blocked.innerHTML=txt_blocked;
+}
+refresh();
+
+function deltask(id){
+    let task=getListTasks();
+    task=task.filter((value)=>value.idTask!=id);
+    saveTask(task);
+    refresh();
 }
